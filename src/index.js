b19a5b9f25aa5c306713928b6987c1cb12778f3d
heke <smileshitou@hotmail.com>
2019-07-24 16:51:31 +0800

    实验

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 module.exports = function(layoutData, options) {
-  // console.log(layoutData); // UI 模块结构组成和样式描述等
-  const renderData = {};
-  var rawJson = JSON.stringify(layoutData)+'';
   
-  // console.log(renderData);
-  renderData.layout = JSON.stringify(layoutData)+'';
+  const renderData = {};
+  renderData.json = JSON.stringify(layoutData);
   
   return {
-    renderData: renderData,
+    renderData,
+    prettierOpt: {},
     panelDisplay: [
       {
-          panelName: 'renderData.layout',
-          panelValue: renderData.layout,
-          panelType: 'string'
+        panelName: 'component.json',
+        panelValue: renderData.json,
+        panelType: 'rawLayout',
+        mode: 'javascript'
       }
     ],
     noTemplate:true
